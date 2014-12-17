d2782650447f36816eae8fc3c1d15e80bbaffa0c
geo242 <gtripoli@gmail.com>
2014-12-16 23:03:20 -0800

    Added code to work through pending high fives

--- src/hats.js
+++ src/hats.js
@@ -23,9 +23,24 @@ function definitelyTimeToBuyMoreHats() {
 }
 
 function highFive(dude, otherDude) {
-	highFives.push(dude, otherDude);
+	highFives.push( {dude, otherDude} );
 }
 
 function getNewShinyHat() {
 	
 }
+
+function processHighFive(highFive) {
+	var dude1 = highFive[0];
+	var dude2 = highFive[1];
+	//TODO - Figure out how to actually high five someone in javascript
+	return true;
+}
+
+window.setInterval(function() {
+	while (highFives.length){
+		if (processHighFive(highFives[0])) {
+			highFives.splice(0, 1);	
+		}
+	}
+}, 60000)
